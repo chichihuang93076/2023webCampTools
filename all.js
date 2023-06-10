dc911c770be18445034853c2b266ccd85d333907
chichi <chichihuang93076@gmail.com>
2023-06-10 15:31:12 +0800

    recommendList swiper.js 輪播

--- all.js
+++ all.js
@@ -1,9 +1,9 @@
-$('.qa-item').click(function(e) {
+$(".qa-item").click(function (e) {
   e.preventDefault();
-  $(this).toggleClass('active');
-  $(this).find('.add-icon').toggleClass('d-none');
-  $(this).find('.remove-icon').toggleClass('d-block');
-  $(this).find('.collapse-content p').toggleClass('show');
+  $(this).toggleClass("active");
+  $(this).find(".add-icon").toggleClass("d-none");
+  $(this).find(".remove-icon").toggleClass("d-block");
+  $(this).find(".collapse-content p").toggleClass("show");
 });
 /*
 $('.qa-item').on("click", () => {
@@ -26,3 +26,40 @@ function gotoTop() {
     behavior: "smooth",
   });
 }
+
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  spaceBetween: 24,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      spaceBetween: 24,
+    },
+  },
+  // 分頁
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // 左右箭頭
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // 滾動條
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+});
