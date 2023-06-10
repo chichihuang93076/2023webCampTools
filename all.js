a985ec0d924d124d3317ba814fe471533780a115
chichi <chichihuang93076@gmail.com>
2023-06-10 14:48:42 +0800

    定價頁 QA 選單摺疊

--- all.js
+++ all.js
@@ -1,3 +1,21 @@
+$('.qa-item').click(function(e) {
+  e.preventDefault();
+  $(this).toggleClass('active');
+  $(this).find('.add-icon').toggleClass('d-none');
+  $(this).find('.remove-icon').toggleClass('d-block');
+  $(this).find('.collapse-content p').toggleClass('show');
+});
+/*
+$('.qa-item').on("click", () => {
+  //e.preventDefault();
+  //console.log(e.target.classList);
+  $(this).toggleClass('active');
+  $(this).find('.add-icon').toggleClass('d-none');
+  $(this).find('.remove-icon').toggleClass('d-block');
+  $(this).find('.collapse-content p').toggleClass('show');
+});
+*/
+
 $(".backtoTop").on("click", () => {
   gotoTop();
 });
